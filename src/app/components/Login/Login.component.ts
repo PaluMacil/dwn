0a803110b69ac0bd3b32d82b5a85c60348359a6a
Dan Wolf <dcwolf@gmail.com>
2017-07-25 23:24:21 -0400

    show login errors

--- src/app/components/Login/Login.component.ts
+++ src/app/components/Login/Login.component.ts
@@ -11,18 +11,32 @@ export class LoginComponent implements OnInit {
 
   email: string;
   password: string;
+  loginErr?: string = null;
 
   login() {
     this.accountAPI.Token(this.email,this.password).subscribe(
       data => {
         localStorage.setItem('dwn_token',data.Token);
         this.accountAPI.session = data;
+        $('#login-modal').modal('hide');
+        this.email = '';
+        this.password = '';
+        this.loginErr = null;
       },
       err => {
-        console.log('Could not log in: ' + JSON.stringify(err));
+        this.loginErr = err.message;
+        this.password = '';
       }
     );
-    
+  }
+
+  logout() {
+    localStorage.removeItem('dwn_token');
+    this.accountAPI.session = null;
+    this.accountAPI.Logout().subscribe(
+      () => { console.log('Logged out') },
+      (err) => { console.log('Could not log out. ' + err.message) }
+    );
   }
 
   ngOnInit() {
