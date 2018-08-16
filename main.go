eba3b55a87a5d067571ed01c58c39c94ccca0e16
Dan Wolf <dcwolf@gmail.com>
2018-08-15 23:50:27 -0400

    refactor filenames and webserver from main

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
 
-	"github.com/PaluMacil/dwn/api/groupapi"
-	"github.com/PaluMacil/dwn/api/infoapi"
-	"github.com/PaluMacil/dwn/api/userapi"
-	"github.com/PaluMacil/dwn/api/usergroupapi"
 	"github.com/PaluMacil/dwn/app"
-	"github.com/PaluMacil/dwn/auth"
 	"github.com/PaluMacil/dwn/setup"
-	"github.com/PaluMacil/dwn/spa"
+	"github.com/PaluMacil/dwn/webserver"
 )
 
 func main() {
@@ -29,47 +19,9 @@ func main() {
 		log.Fatalln("could not ensure app setup:", err)
 	}
 
-	spaModule := spa.New(appModule)
-	authModule := auth.New(appModule)
+	web := webserver.New(appModule)
+	web.Serve()
 
-	userModule := userapi.New(appModule)
-	groupModule := groupapi.New(appModule)
-	usergroupModule := usergroupapi.New(appModule)
-	infoModule := infoapi.New(appModule)
-
-	mux := http.NewServeMux()
-	mux.Handle("/", spaModule)
-	mux.Handle("/oauth/", authModule)
-	mux.Handle("/api/user/", userModule)
-	mux.Handle("/api/group/", groupModule)
-	mux.Handle("/api/usergroup/", usergroupModule)
-	mux.Handle("/api/info/", infoModule)
-
-	srv := &http.Server{
-		Addr:    ":" + appModule.Config.WebServer.Port,
-		Handler: mux,
-		//time from when the connection is accepted to when the request body is fully read
-		ReadTimeout: 5 * time.Second,
-		//time from the end of the request header read to the end of the response write
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-	}
-	stop := make(chan os.Signal)
-	signal.Notify(stop, os.Interrupt)
-	go func() {
-		log.Println("Now serving on port", appModule.Config.WebServer.Port)
-		log.Println(srv.ListenAndServe())
-	}()
-	<-stop
-
-	log.Printf("shutting down ...\n")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal(err)
-	}
 	appModule.Db.Close()
 	log.Printf("Badger: database stopped\n")
 }
---
+++ webserver/webserver.go
@@ -0,0 +1,78 @@
+package webserver
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/PaluMacil/dwn/api/groupapi"
+	"github.com/PaluMacil/dwn/api/infoapi"
+	"github.com/PaluMacil/dwn/api/searchapi"
+	"github.com/PaluMacil/dwn/api/userapi"
+	"github.com/PaluMacil/dwn/api/usergroupapi"
+	"github.com/PaluMacil/dwn/app"
+	"github.com/PaluMacil/dwn/auth"
+	"github.com/PaluMacil/dwn/configuration"
+	"github.com/PaluMacil/dwn/spa"
+)
+
+type WebServer struct {
+	configuration.WebServerConfiguration
+	mux *http.ServeMux
+}
+
+func New(appModule *app.App) *WebServer {
+	spaModule := spa.New(appModule)
+	authModule := auth.New(appModule)
+
+	userModule := userapi.New(appModule)
+	groupModule := groupapi.New(appModule)
+	usergroupModule := usergroupapi.New(appModule)
+	infoModule := infoapi.New(appModule)
+	searchModule := searchapi.New(appModule)
+
+	ws := &WebServer{
+		WebServerConfiguration: appModule.Config.WebServer,
+		mux: http.NewServeMux(),
+	}
+	ws.mux.Handle("/", spaModule)
+	ws.mux.Handle("/oauth/", authModule)
+	ws.mux.Handle("/api/user/", userModule)
+	ws.mux.Handle("/api/group/", groupModule)
+	ws.mux.Handle("/api/usergroup/", usergroupModule)
+	ws.mux.Handle("/api/info/", infoModule)
+	ws.mux.Handle("/api/search/", searchModule)
+
+	return ws
+}
+
+func (ws *WebServer) Serve() {
+	srv := &http.Server{
+		Addr:    ":" + ws.Port,
+		Handler: ws.mux,
+		//time from when the connection is accepted to when the request body is fully read
+		ReadTimeout: 5 * time.Second,
+		//time from the end of the request header read to the end of the response write
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+	go func() {
+		log.Println("Now serving on port", ws.Port)
+		log.Println(srv.ListenAndServe())
+	}()
+	<-stop
+
+	log.Printf("shutting down ...\n")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
