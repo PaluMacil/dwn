d244fb34fd223484239ac3e635085f563b9b5242
Dan Wolf <dcwolf@gmail.com>
2019-03-28 23:25:55 -0400

    add echo server

---
+++ cmd/echo/echo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+type EchoModule struct {
+	history *History
+}
+
+func (mod EchoModule) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "%v", err)
+	}
+	request := string(requestDump)
+	mod.history.add(request)
+	_, _ = fmt.Fprintf(w, request)
+}
---
+++ cmd/echo/history.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sort"
+	"sync"
+	"time"
+)
+
+type History []Record
+
+var historyLock = &sync.Mutex{}
+
+func (h *History) add(request string) {
+	history := *h
+	record := Record{request, time.Now()}
+	historyLock.Lock()
+	if len(history) >= 100 {
+		_, history = history[0], history[1:]
+	}
+	*h = append(history, record)
+	historyLock.Unlock()
+}
+func (h History) write(w http.ResponseWriter) error {
+	historyLock.Lock()
+	defer historyLock.Unlock()
+	sort.Slice(h, func(i, j int) bool {
+		return h[i].Date.UnixNano() > h[j].Date.UnixNano()
+	})
+	err := json.NewEncoder(w).Encode(h)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "%v", err)
+	}
+	return nil
+}
+
+type Record struct {
+	Request string    `json:"request"`
+	Date    time.Time `json:"date"`
+}
+
+type HistoryModule struct {
+	history *History
+}
+
+func (mod HistoryModule) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		return
+	} else if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	err := mod.history.write(w)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "%v", err)
+	}
+}
